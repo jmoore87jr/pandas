af4c6f48de9f2bc6246ffeffa995523f65c8cbd9
jmoore87jr <base_1187@yahoo.com>
2021-06-10 10:38:38 -0700

    save_to_rds() added

--- logic_merge.py
+++ logic_merge.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import credentials
 import s3fs
+from sqlalchemy import create_engine
+import psycopg2
+from credentials import *
 
 # TODO: have load_from_s3 take *args and return a list of dataframes of any length
 
@@ -11,7 +13,6 @@ import s3fs
 """
 
 def load_from_s3():
-    
    # pipe delimited
     df1 = pd.read_csv('s3://mnnk/pipe_data.csv', sep='|')
 
@@ -22,7 +23,6 @@ def load_from_s3():
 
 
 def merge_dfs(df1, df2):
-
     # concatenate the 3 separate ID rows in df1 and remove the partial ID columns
     df1.insert(0, 'ID', df1['ID1'].astype('str') + df1['ID2'] + df1['ID3'])
     df1 = df1[['ID', 'A', 'B']]
@@ -34,8 +34,42 @@ def merge_dfs(df1, df2):
 
     return result
 
+def save_to_rds(df):
+    try:
+        # connect to database
+        conn = psycopg2.connect(
+            host=DATABASE_ENDPOINT,
+            database=DATABASE_NAME,
+            user=USERNAME,
+            password=PASSWORD,
+            port=PORT
+        )
+        print("Connected to database")
+
+        # create engine
+        engine = create_engine(f'postgresql://{USERNAME}:{PASSWORD}@{DATABASE_ENDPOINT}:{PORT}/{DATABASE_NAME}')
+
+        # insert data
+        try:
+            df.to_sql('table1', con=engine, if_exists='append', chunksize=20000)
+        except:
+            print("Data format doesn't match database format")
+
+        # commit to database
+        conn.commit()
+        print("Saved to database")
+
+        # close connection
+        conn.close()
+        print("Connection closed")
+
+    except psycopg2.OperationalError as e:
+        print(e)
+
 
 if __name__ == "__main__":
     dfs = load_from_s3()
 
-    merge_dfs(dfs[0], dfs[1])
+    df = merge_dfs(dfs[0], dfs[1])
+
+    save_to_rds(df)
