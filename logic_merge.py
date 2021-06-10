a44d437c2839e2f895c6bc9eb3b5b722991e2424
jmoore87jr <base_1187@yahoo.com>
2021-06-10 09:59:44 -0700

    load data from S3

--- logic_merge.py
+++ logic_merge.py
@@ -1,28 +1,41 @@
-import pandas as pd 
+import pandas as pd
+import credentials
+import s3fs
 
-# TODO: retreive the data from S3, and write it out to RDS postgres database
+# TODO: have load_from_s3 take *args and return a list of dataframes of any length
 
 """ 
-* boilerplate for joining DSAMS and CISIL data using Pandas
+* join DSAMS and CISIL data from S3 using Pandas
 * DSAMS data is pipe delimited and the ID columns need to be combined to match the CISIL ID column
 * CISIL data is fixed width
 """
 
-# read pipe-delimited data
-# NOTE: include "header=<n>" in the read_csv call to deal with headers
-df1 = pd.read_csv('data/pipe_data.csv', sep='|')
+def load_from_s3():
+    
+   # pipe delimited
+    df1 = pd.read_csv('s3://mnnk/pipe_data.csv', sep='|')
 
-# read fixed width data
-df2 = pd.read_fwf('data/fw_data.csv')
+    # fixed width
+    df2 = pd.read_fwf('s3://mnnk/fw_data.csv')
 
-print(df1.head(3))
-print(df2.head(3))
+    return [df1,df2]
 
-# concatenate the 3 separate ID rows in df1 and remove the partial ID columns
-df1.insert(0, 'ID', df1['ID1'].astype('str') + df1['ID2'] + df1['ID3'])
-df1 = df1[['ID', 'A', 'B']]
 
-# merge the dataframes on ID and sort by ID
-result = df1.merge(df2, left_on='ID', right_on='ID').sort_values(by='ID')
+def merge_dfs(df1, df2):
 
-print(result)
+    # concatenate the 3 separate ID rows in df1 and remove the partial ID columns
+    df1.insert(0, 'ID', df1['ID1'].astype('str') + df1['ID2'] + df1['ID3'])
+    df1 = df1[['ID', 'A', 'B']]
+
+    # merge the dataframes on ID and sort by ID
+    result = df1.merge(df2, left_on='ID', right_on='ID').sort_values(by='ID')
+
+    print(result)
+
+    return result
+
+
+if __name__ == "__main__":
+    dfs = load_from_s3()
+
+    merge_dfs(dfs[0], dfs[1])
