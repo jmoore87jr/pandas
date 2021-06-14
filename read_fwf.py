3e826b2232261cd961f6ee0c5aa409f61bee78d1
John <base_1187@yahoo.com>
2021-06-14 12:08:17 -0400

    update for reading fixed width files

---
+++ read_fwf.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+"""
+Alter the load_from_s3() function in main.py to read column widths and names
+"""
+
+colnames = ['col1', 'col2', 'col3']
+
+# read in the fixed width file
+# 'widths' takes a list of column widths
+# 'names' takes a list of column names
+df = pd.read_fwf('data/L02.TEST3.FILE00.NAME', widths=[5, 15, 12], names=colnames)
+
+print(df)
